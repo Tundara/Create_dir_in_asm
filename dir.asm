6db4ffd090f65d70ba18d3db2dcd946955c2170b
Tundara <58626539+TundaraPhP@users.noreply.github.com>
2021-05-11 18:46:00 +0200

    Add files via upload

---
+++ dir.asm
@@ -0,0 +1,45 @@
+BITS 64
+
+global _start
+
+section .rodata 
+    ask db "Choose a folder name : "
+    ask_len equ $-ask
+
+section .bss
+    pathname resb 16
+
+section .text
+
+
+_start:
+
+    call _ask_name_string
+    call _ask_fold_name
+    call _create_folder
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+_ask_name_string:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, ask
+    mov rdx, ask_len
+    syscall
+    ret
+
+_create_folder:
+    mov rax, 83
+    mov rdi, pathname
+    syscall
+    ret
+
+_ask_fold_name:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, pathname
+    mov rdx, 16
+    syscall
+    ret
